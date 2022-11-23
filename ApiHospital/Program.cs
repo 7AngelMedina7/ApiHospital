ddc5bfd1b506e4e64ad3897cd4bc2b45f25b56d9
7AngelMedina7 <anjair@live.com.mx>
2022-11-22 21:18:16 -0600

    Creacion de Servicios (para la creacion del txt), Middlewares y añadir los los services, junto los logs.

--- ApiHospital/Program.cs
+++ ApiHospital/Program.cs
@@ -2,16 +2,13 @@ using ApiHospital;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-// Add services to the container.
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
+
 var app = builder.Build();
 
 var serviceLogger = (ILogger<Startup>)app.Services.GetService(typeof(ILogger<Startup>));
 
-
-startup.Configure(app, app.Environment);
-
+startup.Configure(app, app.Environment, serviceLogger);
 
 app.Run();
